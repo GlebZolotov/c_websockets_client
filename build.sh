ca99f45cbf6ffce4b111859277fbe270594dda4f
GlebZolotov <IPGZ-97@yandex.ru>
2023-09-23 12:53:07 +0400

    add_params

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ git clone "https://github.com/warmcat/libwebsockets.git"
 
 cd libwebsockets && mkdir build && cd build
 
-cmake .. -DLWS_WITHOUT_EXTENSIONS=OFF -DLWS_LINK_TESTAPPS_DYNAMIC=ON && make
+CFLAGS="-I$1" LDFLAGS="-L$2" cmake .. -DZLIB_LIBRARY=$2/libz.a -DZLIB_INCLUDE_DIR=$1 -DLWS_WITHOUT_EXTENSIONS=OFF -DLWS_LINK_TESTAPPS_DYNAMIC=ON && make
 
 cd $BASEDIR && make
 
