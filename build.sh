c16d9af530165eedc01b101d9cbeacce9a27c4d4
GlebZolotov <IPGZ-97@yandex.ru>
2023-09-23 12:44:39 +0400

    back_to_start_state

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ git clone "https://github.com/warmcat/libwebsockets.git"
 
 cd libwebsockets && mkdir build && cd build
 
-cmake .. -DZLIB_LIBRARY=$1 -DZLIB_INCLUDE_DIR=$2 -DLWS_WITHOUT_EXTENSIONS=OFF -DLWS_LINK_TESTAPPS_DYNAMIC=ON && make
+cmake .. -DLWS_WITHOUT_EXTENSIONS=OFF -DLWS_LINK_TESTAPPS_DYNAMIC=ON && make
 
 cd $BASEDIR && make
 
