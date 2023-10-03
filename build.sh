3d69ba94e61a594932524d71b7caa8682ff4a1f9
GlebZolotov <IPGZ-97@yandex.ru>
2023-10-03 13:18:41 +0400

    add_keys_for_binarybuilder

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ git clone "https://github.com/warmcat/libwebsockets.git"
 
 cd libwebsockets && mkdir build && cd build
 
-CFLAGS="-I$1" LDFLAGS="-L$2" cmake .. -DZLIB_LIBRARY=$2/libz.a -DZLIB_INCLUDE_DIR=$1 -DLWS_WITHOUT_EXTENSIONS=OFF -DLWS_LINK_TESTAPPS_DYNAMIC=ON && make
+CFLAGS="-I$1" LDFLAGS="-L$2" cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DZLIB_LIBRARY=$2/libz.a -DZLIB_INCLUDE_DIR=$1 -DLWS_WITHOUT_EXTENSIONS=OFF -DLWS_LINK_TESTAPPS_DYNAMIC=ON && make
 
 cd $BASEDIR && make
 
